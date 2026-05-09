f111b19a4ef3dc2b10f94bb57c26472be89f0d98
Roger Pollock <RPOLLOCK@augusta.edu>
2026-05-08 21:57:06 -0400

    commit

--- script.js
+++ script.js
@@ -7,17 +7,19 @@ window.addEventListener('scroll', function () {
   }
 });
 
-fetch('links.json')
-  .then(res => res.json())
-  .then(data => {
-    document.querySelectorAll('[data-link]').forEach(a => {
-      const id = a.dataset.link;
-      const linkObj = data.links.find(l => l.id === id);
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('./links.json')
+    .then(res => res.json())
+    .then(data => {
+      document.querySelectorAll('[data-link]').forEach(a => {
+        const id = a.dataset.link;
+        const linkObj = data.links.find(l => l.id === id);
 
-      if (!linkObj) return;
+        if (!linkObj) return;
 
-      a.href = linkObj.url;
-      a.target = linkObj.target;
-      a.rel = "noopener";
+        a.href = linkObj.url;
+        a.target = linkObj.target;
+        a.rel = "noopener";
+      });
     });
-  });
+})
